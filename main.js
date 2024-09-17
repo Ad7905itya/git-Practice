c41b5ecec7612966db0156a710227f2b30ecf0f3
Aditya singh <6306043783s@gmail.com>
2024-09-17 19:23:36 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// console.log(addTwoNumber(14,25));
+
+// const addTwoNumber = (a,b) => {
+//     return a + b
+// }
+
+// console.log(addTwoNumber(5,5));
+
+
+// console.log('5' + 3);
+// console.log('5' - 3);
+
---
+++ userData.js
@@ -0,0 +1,24 @@
+const userData = [
+    {
+        firstName: 'Aditya',
+        lastName: 'Singh',
+        age: 18
+    },
+    {
+        firstName: 'Aman',
+        lastName: 'Mishra',
+        age: 16
+    },
+    {
+        firstName: 'Akash',
+        lastName: 'Gupta',
+        age: 19
+    },
+    {
+        firstName: 'Nilam',
+        lastName: 'Tiwari',
+        age: 16
+    }
+]
+
+export {userData};
